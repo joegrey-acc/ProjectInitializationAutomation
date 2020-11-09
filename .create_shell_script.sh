39eb9e783ea09b987cb721bfd067e3718b514769
Joe Grey <joegrey1990@gmail.com>
2020-11-09 09:55:25 +0000

    Commit

---
+++ .create_shell_script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# source C:/Users/joegr/Documents/Projects/ProjectInitializationAutomation/.create_shell_script.sh
+function create() {
+    python C:/Users/joegr/Documents/Projects/ProjectInitializationAutomation/create.py $1
+}
