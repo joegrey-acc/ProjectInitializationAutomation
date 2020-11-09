61e6897689203892b9c30e6185d85d0481e8f5dc
Joe Grey <joegrey1990@gmail.com>
2020-11-09 10:00:59 +0000

    adding correct files

---
+++ README.md
@@ -0,0 +1,61 @@
+# ProjectInitializationAutomation
+
+## This will allow user to create a project in a specified folder and create a repo in GitHub and have it initialised with a ReadMe with the command :
+### create <project_name>
+
+## Instructions:
+(ensure all folder path have forward slashes "/" - NOT back slashes "\")
+1. CREATE .env file to project folder
+2. Add FILEPATH = "< folder where you want to create new project >" to .ev
+3. Add GITHUB_ACCESS_TOKEN = "< the access token that has all repo elements checked >" to .ev
+
+### Creating functions / commands in terminals
+#### WINDOWS
+4. CREATE a create.bat file
+5. ADD the following code to the bat file:
+    ```
+        start cmd /k "<folder where project is located>\create.py %1"
+    ```
+6. Add .bat file to PATH in the environmental variables
+7. Now you can successfully create a new project from anywhere in a windows termainal using the command:
+    ```
+        create <project name>
+    ```
+
+#### BASH
+4. OPEN ~/.bashrc in an editor, e.g.:
+    ```
+        code ~/.bashrc
+    ```
+    This file will run all alias' when bash is opened 
+5. ADD the following command to the file:
+    ```
+        alias create="python <folder where project is located>/create.py $1"
+    ```
+6. Now you can successfully create a new project from anywhere in a bash termainal using the command:
+    ```
+        create <project name>
+    ```
+
+## Additional Feature:
+### Easy switch between bash & windows terminals
+#### Windows to Bash
+1. CREATE bash.bat file
+2. ENSURE folder in which .bat file is located is in PATH in the environmental variable
+3. ADD the following code into bash.bat file:
+    (Where your git folder is stored)
+    ```
+        "C:\Program Files\Git\bin\sh.exe" --login -i
+    ```
+4. NOW in a windows terminal when you type the command 'bash', the terminal will switch to a bash terminal
+
+#### Bash to Windows
+1. OPEN ~/.bashrc in an editor, e.g.:
+    ```
+        code ~/.bashrc
+    ```
+2. ADD the following command to the file:
+    ```
+        alias cmd="C:/WINDOWS/System32/cmd.exe"
+    ```
+3. NOW in a bash terminal when you type the command 'cmd', the terminal will switch to a windows terminal
---
+++ create.py
@@ -0,0 +1,75 @@
+import sys
+import os
+from github import Github
+from dotenv import load_dotenv
+
+load_dotenv()
+
+path = os.getenv("FILEPATH")
+githubAccessToken = os.getenv("GITHUB_ACCESS_TOKEN")
+g = Github(githubAccessToken)
+
+# This function checks if a Repo in GitHub or folder on PC shares the same name with the project you are trying to create
+def canCreate():
+    repo_exists = False
+    for repo in g.get_user().get_repos():
+        if str(repo.name).upper() == str(sys.argv[1]).upper():
+            repo_exists = True
+            print("Cannot create project because a REPO with the same name exists in GitHub")
+
+    folder_exists = False
+    for filename in os.listdir(path):
+        if str(filename).upper() == str(sys.argv[1]).upper():
+            folder_exists = True
+            print("Cannot create project because a FOLDER with the same name exists in the path")
+
+    if repo_exists == True or folder_exists == True:
+        print("ERROR cannot create Project")
+    else:
+        createRepo()
+
+# Creates Repo in GitHub
+def createRepo():
+    try:
+        g.get_user().create_repo(sys.argv[1])
+    except Exception as e:
+        print("ERROR IN CREATING REPO")
+        print(e)
+    else:
+        print("REPO CREATED")
+        createFolder()
+
+# Creates folder on PC in specified location
+def createFolder():
+    try:
+        os.makedirs(path + "\\"+str(sys.argv[1]), 0o777)
+    except Exception as e:
+        print("ERROR IN CREATING FOLDER")
+        print(e)
+    else:
+        print("FOLDER CREATED")
+        initFolder()
+
+# Initialises the folder & pushes it up to GitHub
+def initFolder():
+    command_to_execute = 'cd ' + str(path) + '\\' + str(sys.argv[1]) + ' && echo # '+ str(sys.argv[1]) + ' >> README.md' + ' && git init && git remote add origin https://github.com/' + g.get_user().login + '/' + str(sys.argv[1]) + ' && git add * && git commit -m "Initial commit" && git push -u origin master'
+    os.system(command_to_execute)
+    print(str(sys.argv[1]) + ' folder initialised')
+    os.system('code ' + str(path) + '\\' + str(sys.argv[1]))   
+
+
+if __name__ == "__main__":
+    canCreate()
+    #deletion()
+
+'''
+# Deletes repo in GitHub - Access Token needs to allow deletion
+def deletion():
+    try:
+        g.get_user().get_repo(str(sys.argv[1])).delete()
+    except Exception as e:
+        print("ERROR IN DELETING REPO: ")
+        print(e)
+    else:
+        print("REPO DELETED")
+'''
