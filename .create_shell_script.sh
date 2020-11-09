c1763832a81e4a86af8ca7372649501c0cb43ebb
Joe Grey <joegrey1990@gmail.com>
2020-11-09 09:58:29 +0000

    fixing .gitignore

--- .create_shell_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# source C:/Users/joegr/Documents/Projects/ProjectInitializationAutomation/.create_shell_script.sh
-function create() {
-    python C:/Users/joegr/Documents/Projects/ProjectInitializationAutomation/create.py $1
-}
